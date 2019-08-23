1113ea4a71f8a7d11c385d3406dc8e8761f8f099
aragon598 <48097234+aragon598@users.noreply.github.com>
2019-08-23 00:06:58 -0600

    Add files via upload

--- Ejercicio#3.c
+++ Ejercicio#3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+
 int main(){
 int tamanio;
 
@@ -12,6 +13,33 @@ printf("Ingrese el número de datos a ingresar.");
 			printf("Ingrese el valor N° %d",(i+1));
 			scanf("%e",&vector[i]);
 		}
+		
+		//media
+		float aux=0.0;
+		float media;
+		for (int i = 0; i < tamanio; i++)
+		{
+			aux=(aux + vector[i]); 
+		}
+		media=(aux/tamanio);
+		
+		printf("\nLa media es : %e",media);
+		
+		//varianza
+		
+		float var=0.0;
+		for (int i = 0; i < tamanio; i++)
+		{
+			var += pow(vector[i]-media,2);
+		}
+		var=(var/tamanio);
+		printf("\nLa varianza es %e",var);
+		
+		//Desviacion estandar
+		
+		float dev=sqrt(var);
+		printf("\nLa desviación es %e",dev);
+		
 		
 		
 		
